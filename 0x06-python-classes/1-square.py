7ead2ad4f565518f6988c1e2a327f64adbf40494
Mufidat-Ahmed <imufidatahmad@gmai.com>
2023-08-02 12:00:00 -0700

    square 2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""Define clss square"""
+
+
 class Square:
+    """Represent square"""
+
     def __init__(self, size):
+        """
+        Args:
+            size(int): size of square
+        """
+
         self.__size = size
