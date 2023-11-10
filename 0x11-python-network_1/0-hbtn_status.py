780806cf7f7c3d4278a9fd24f4127cd76f54bf8a
Mufidat-Ahmed <imufidatahmad@gmail.com>
2023-11-10 04:35:26 -0800

    What is my status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+
+
+url = "https://alx-intranet.hbtn.io/status"
+
+
+with urllib.request.urlopen(url) as response:
+    content = response.read()
+    print("Body response:")
+    print("\t- type: type(content)")
+    print("\t- content: content")
+    print("\t- utf8 content: content.decode('utf-8')")
