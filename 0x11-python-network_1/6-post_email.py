b5f761a6f09f1a09fef1a5ffd28567a916cfdbe8
Mufidat-Ahmed <imufidatahmad@gmail.com>
2023-11-10 07:01:46 -0800

    Post an email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""sends a POST request to the passed URL with the email as a parameter
+displays the body of the response.
+"""
+
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = {"email": email}
+    res = requests.post(url, data=data)
+    print(res.text)
